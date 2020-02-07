4eef2a04b4f7577e4cceaee8074efb1adc2a4a93
David Woolsey <woolsey2316@gmail.com>
2020-02-08 00:11:04 +1100

    improved landing page design navigation component

--- moviedatabase/src/App.js
+++ moviedatabase/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import {Navbar,Nav} from 'react-bootstrap';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import MovieSearch from './components/MovieSearch.js';
 import ActorSearch from './components/ActorSearch.js';
 import TelevisionSearch from './components/TelevisionSearch.js';
 import Home from './components/Home.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from './components/Navigation.js'
+import Movie from './components/Movie.js';
 
 class App extends Component {
 
@@ -41,16 +41,7 @@ class App extends Component {
       return (
         <Router>
           <div className="App">
-          <Navbar className="navbar navbar-dark bg-dark" bg="primary" variant="dark">
-            <Navbar.Brand href="#home">Tv&MovieDb</Navbar.Brand>
-            <Nav className="navbar navbar-dark bg-dark">
-              <Nav.Link href="home">Home</Nav.Link>
-              <Nav.Link href="movie">Movie</Nav.Link>
-              <Nav.Link href="television">Television</Nav.Link>
-              <Nav.Link href="actor">Actor</Nav.Link>
-            </Nav>
-          </Navbar>
-          <div className="search-result-canvas">{this.state.rows}</div>
+          <Navigation/>
             <Switch>
               <Route exact path="/" ><Home/></Route>
               <Route exact path="/home" ><Home/></Route>
